34ae6a91af5eed9718ff55e79e8ba284c60ce721
Demonstr66 <demonstr66@yandex.ru>
2022-02-28 16:56:35 +0300

    initial

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    <v-btn
+  elevation="2"
+></v-btn>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+
+    data: () => ({
+      
+    }),
+  }
+</script>
