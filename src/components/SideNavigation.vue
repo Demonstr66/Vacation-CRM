70a9cc82c483d699cbb84523efd832c4ecaf3a3f
Demonstr66 <demonstr66@yandex.ru>
2022-03-01 15:45:12 +0300

    vuetify calendar

---
+++ src/components/SideNavigation.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-navigation-drawer app permanent>
+    <v-list nav dense>
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-calendar-month-outline</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>График отпусков</v-list-item-title>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-folder</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>My Files</v-list-item-title>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-account-multiple</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Shared with me</v-list-item-title>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-star</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Starred</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "SideNavigation",
+
+  data: () => ({}),
+};
+</script>
